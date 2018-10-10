e495da078dec279b1fc95a930f3233cc3fafaf99
Konrad Kołakowski <vashpan@gmail.com>
2018-10-10 21:35:26 +0200

    Initial Commit

---
+++ Pixlr/Pixlr.h
@@ -0,0 +1,19 @@
+//
+//  Pixlr.h
+//  Pixlr
+//
+//  Created by Konrad Kołakowski on 10/10/2018.
+//  Copyright © 2018 One Minute Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Pixlr.
+FOUNDATION_EXPORT double PixlrVersionNumber;
+
+//! Project version string for Pixlr.
+FOUNDATION_EXPORT const unsigned char PixlrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pixlr/PublicHeader.h>
+
+
