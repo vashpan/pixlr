4b140e7e417ba2c492440ed2fb70ee2c7598f3b6
Konrad Kołakowski <vashpan@gmail.com>
2018-11-27 23:24:16 +0100

    Added helper property to get vector_uint2 size of Size

--- Pixlr/Platform/macOS/Helpers/Size+macOS.swift
+++ Pixlr/Platform/macOS/Helpers/Size+macOS.swift
@@ -14,6 +14,10 @@ internal extension Size {
         return CGSize(width: CGFloat(self.width), height: CGFloat(self.height))
     }
     
+    internal var simdSize: vector_uint2 {
+        return vector_uint2(UInt32(self.width), UInt32(self.height))
+    }
+    
     // MARK: Initialization
     init(cgSize: CGSize) {
         self.init(width: Float(cgSize.width), height: Float(cgSize.height))
