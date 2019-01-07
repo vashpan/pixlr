cf613c455d20b71a0e7c1c48b9c78abd53c18488
Konrad Kołakowski <vashpan@gmail.com>
2019-01-08 00:06:32 +0100

    Platform: Color extension for macOS platform to handle color as vector_float4

---
+++ Pixlr/Platform/macOS/Helpers/Color+macOS.swift
@@ -0,0 +1,16 @@
+//
+//  Color+macOS.swift
+//  Pixlr
+//
+//  Created by Konrad Kołakowski on 07/01/2019.
+//  Copyright © 2019 One Minute Games. All rights reserved.
+//
+
+import CoreGraphics
+
+internal extension Color {
+    // MARK: Properties
+    internal var simdColor: vector_float4 {
+        return vector_float4(Float(self.r) / 255.0, Float(self.g) / 255.0, Float(self.b) / 255.0, Float(self.a) / 255.0)
+    }
+}
